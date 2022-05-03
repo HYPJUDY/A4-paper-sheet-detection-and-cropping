e10c06f61d4b7d4c94c232a1074390fa36ccbb43
Raphaël Droz <raphael.droz@gmail.com>
2022-05-03 14:28:02 -0300

    Accept input filenames from arguments

--- main.cpp
+++ main.cpp
@@ -7,45 +7,21 @@
 #  Code        : https://github.com/HYPJUDY/A4-paper-sheet-detection-and-cropping
 */
 
+#include <limits.h>
 #include "Warping.h"
-int main() {
-	int CASE = 1; // 1 for dataset1; otherwise for dataset2
 
-	/* Parameters for dataset */
-	int image_num = 16;
-	const char* data_folder = "dataset1/";
-	if (CASE == 1) {
-		image_num = 16;
-		data_folder = "dataset1/";
-	}
-	else {
-		image_num = 5;
-		data_folder = "dataset2/";
-	}
-	
-	// adjust the num array below to process different image
-	std::vector<const char*> num = { "0", "1", "2", "3", "4", "5",
-		"6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" };
-	for (int i = 0; i < image_num; ++i) {
-		// load source image
-		char inPath[80];
-		strcpy(inPath, data_folder);
-		strcat(inPath, num[i]);
-		strcat(inPath, ".bmp");
-		
+int main(int argc, char **argv) {
+	char inPath[NAME_MAX], outPath[NAME_MAX], outPath2[NAME_MAX];
+
+	for (int i = 1; i < argc; ++i) {
+		strncpy(inPath, argv[i], NAME_MAX - 1);
+		sprintf(outPath, "/tmp/output-%02d.jpg", i);
+		sprintf(outPath2, "/tmp/output_A4-%02d.jpg", i);
+
 		Hough hough(inPath);
 		Warping Warping(hough);
 		
-		char outPath[80];
-		strcpy(outPath, data_folder);
-		strcat(outPath, num[i]);
-		strcat(outPath, "_marked.bmp");
 		hough.getMarkedImg().display().save(outPath);
-
-		char outPath2[80];
-		strcpy(outPath2, data_folder);
-		strcat(outPath2, num[i]);
-		strcat(outPath2, "_A4.bmp");
 		Warping.getCroppedImg().display().save(outPath2);
 	}
 	return 0;
